bd935a220171e103e32e901bba552b479f069594
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-05 21:44:29 +0530

    #include<stdio.h> int main() {     int num1,num2,num3;     scanf("%d%d",&num1,&num2);     if(num1>num2 && num1-num2);     {         num3=num1-num2;         printf("%d",num3);     } }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    scanf("%d%d",&num1,&num2);
+    if(num1>num2 && num1-num2);
+    {
+        num3=num1-num2;
+        printf("%d",num3);
+    }
+}
