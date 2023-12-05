55a0124339dac74f679edd8b17ca2f1ae7a720b5
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-05 21:33:19 +0530

    #include<stdio.h> int main() {     int num1,num2,num3;     scanf("%d%d",&num1,&num2);     if(num1<num2 && num2-num1)     {         num3=num2-num1;     printf("%d",num3);     }
    }

---
+++ Donation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    scanf("%d%d",&num1,&num2);
+    if(num1<num2 && num2-num1)
+    {
+        num3=num2-num1;
+    printf("%d",num3);
+    }
+    
+}
