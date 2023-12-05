6d6bd2b1b44d7cef74c2a2371bf3b10fa4bbeeb3
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-05 22:00:22 +0530

    #include<stdio.h> int main() {     int num1,num2,num3,num4;     scanf("%d%d%d%d",&num1,&num2,&num3,&num4);     if(num1>=10 && num2>=10 && num3>=10 && num4>=10)     {
            printf("YES");
        }
        else
        printf("NO");
    }

---
+++ Target.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,num4;
+    scanf("%d%d%d%d",&num1,&num2,&num3,&num4);
+    if(num1>=10 && num2>=10 && num3>=10 && num4>=10)
+    {
+        
+        printf("YES");
+    }
+    else
+    printf("NO");
+}
