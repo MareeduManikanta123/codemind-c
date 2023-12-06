6c47277c553e9b1b6237ff66f43ca380fb5cb832
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-06 14:37:56 +0530

    #include<stdio.h> int main() {     int n,m,k,a,b,c,d;     scanf("%d%d%d%d%d%d%d",&n,&m,&k,&a,&b,&c,&d);     if(k-a>=m)     {         printf("YES ");     }     else     {         printf("NO ");     }      if(c-d>=b)     {         printf("YES ");     }     else     {         printf("NO ");     } }

--- Student_Registration.c
+++ Student_Registration.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 int main()
-
 {
-    int n,m,k,i,a;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int n,m,k,a,b,c,d;
+    scanf("%d%d%d%d%d%d%d",&n,&m,&k,&a,&b,&c,&d);
+    if(k-a>=m)
     {
-        scanf("%d%d%d",&a,&m,&k);
-        if(m-k>=a)
-        {
-            printf("YES
+        printf("YES
 ");
-            
-        }
-        else
-        {
-            printf("NO
+    }
+    else
+    {
+        printf("NO
 ");
-        }
+    }
+     if(c-d>=b)
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
     }
 }
