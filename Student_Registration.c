2761b0d9ef8dc1d004c561953951d56855ff72b5
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-06 14:20:44 +0530

    #include<stdio.h> int main() {     int n,m,k,i,a;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d%d%d",&a,&m,&k);          if(m-k>=a)          {              printf("YES ");          }         else          {              printf("NO ");          }     }
    }

---
+++ Student_Registration.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,k,i,a;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d%d%d",&a,&m,&k);
+         if(m-k>=a)
+         {
+             printf("YES
+");
+         }
+        else
+         {
+             printf("NO
+");
+         }
+    }
+   
+}
