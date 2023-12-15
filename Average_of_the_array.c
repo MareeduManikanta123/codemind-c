bba4509a81eddc5de48dd65014cdf19d9b4f4d2b
MareeduManikanta123 <147792309+MareeduManikanta123@users.noreply.github.com>
2023-12-15 20:54:58 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     float avg;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         sum=sum+arr[i];
        }
         avg=sum/(float)n;
        printf("%.2f",avg);
    }

---
+++ Average_of_the_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    float avg;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+        
+    }
+     avg=sum/(float)n;
+    printf("%.2f",avg);
+}
